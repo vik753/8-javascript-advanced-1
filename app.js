7cc35b14a2e4c42e8d96ca94d4e8e5c51c3e41db
IhorKorenets <vik753@gmail.com>
2023-03-01 15:17:43 +0200

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('Hello')
+let weatherMap = new Map([
+	['London', 10],
+	['Kyiv', 7],
+	['Paris', 14]
+]);
+
+weatherMap = new Map([...weatherMap].map(v => v.reverse()))
+
+console.log(weatherMap);
